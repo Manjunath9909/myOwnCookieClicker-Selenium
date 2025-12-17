444719f46a735fd19344804fae17f7ea603fad8c
Manjunath C <manjunathmc1996@gmail.com>
2025-12-17 15:38:28 +0530

    trying to build a multi threaded workload for multiple scripts, starts now

---
+++ runTests.py
@@ -0,0 +1,13 @@
+import countercheck
+import buffcheck
+import sys
+
+setupString = sys.argv[1]
+
+def validateSetUp():
+    #check to see in the given test setup string is all good
+    return
+
+def runSetup():
+    #do the setup using the given commands in the setUp string
+    return
