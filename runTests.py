1c526d988919f44ee003dad040c8ee2133ecdf46
Manjunath C <manjunathmc1996@gmail.com>
2025-12-17 23:26:39 +0530

    only testcases remain. threading is done

--- runTests.py
+++ runTests.py
@@ -3,32 +3,59 @@ from testcases import countercheck, reset, buffcheck
 import sys
 import threading
 
+#globals
+setupString = sys.argv[1]
+#add new valid args to this list
+validArgs = ["-cc", "-b", "-r"]
+threads = [None]*10
+
 def validateSetUp():
     #check to see in the given test setup string is all good
+    for i in range(1, sys.argv.__len__()):
+        if isValidArg(sys.argv[i]):
+            continue
+        else:
+            print("Invalid arguement : " + sys.argv[i])
+            print("Valid arguements are : " + str(validArgs))
+            exit(0)
+    runSetup()
     return
 
 def runSetup():
     #do the setup using the given commands in the setUp string
     for i in range(1, sys.argv.__len__()):
-        threads[i-1] = buildThreads(sys.argv[i])
+        buildThreads(sys.argv[i], i-1)
     startTest()
     return
 
 def startTest():
+
     for i in range(0, sys.argv.__len__()-1):
         threads[i].start()
     for i in range(0, sys.argv.__len__()-1):
-        threads[i].join()
-    print("Started all the testcases")    
+        threads[i].join() 
 
-def buildThreads(arg):
+#the function targetted by all threads
+def threadFunc(arg, index):
     methodDict = {
         "-b": buffcheck,
         "-cc": countercheck,
         "-r" : reset
     }
-    return threading.Thread(target=methodDict.get(arg, lambda: "unknown").run1(), args="manju", name="Threadx")
+    thisWindowPosition = getPositionData(index)
+    methodDict.get(arg, lambda: "unknown").run(thisWindowPosition, 210)
 
-setupString = sys.argv[1]
-threads = [None]*10
-runSetup()
+def buildThreads(arg, index):
+    threads[index] = threading.Thread(target=threadFunc, args=(arg,index,))
+
+def isValidArg(arg):
+    if arg in validArgs:
+        return True
+
+def getPositionData(windowCount):
+    snapNum = 450 * windowCount
+    nextWindowPosition = [300,600,snapNum,0]
+    return nextWindowPosition
+
+#flow starts here
+validateSetUp()
--- testcases/countercheck.py
+++ testcases/countercheck.py
@@ -1,52 +1,61 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 import time
+def run(winPosSize,  finalExpectedCookieJar):
 
-def run(positionX, positionY, sizeWidth, sizeHeight, finalExpectedCookieJar):
-    service = Service(executable_path="chromedriver.exe")
+    sizeWidth = winPosSize[0]
+    sizeHeight = winPosSize[1]
+    positionX = winPosSize[2]
+    positionY = winPosSize[3]
+
+    service = Service(executable_path="testcases/chromedriver.exe")
     driver = webdriver.Chrome(service=service)
     driver.get("file:///E:/projects/selenium2/index.html")
+
+    #element declaration
     bigCookie = driver.find_element(By.ID, "cookie")
     x2multiplier = driver.find_element(By.ID, "x2")
     x3multiplier = driver.find_element(By.ID, "x3")
     x4multiplier = driver.find_element(By.ID, "x4")
     x5multiplier = driver.find_element(By.ID, "x5")
     x6multiplier = driver.find_element(By.ID, "x6")
+
+    #set window size and position
     driver.set_window_size(sizeWidth, sizeHeight)
     driver.set_window_position(positionX, positionY)
     time.sleep(2)
-    clickCookieFor(10)
+    clickCookieFor(bigCookie,10)
     x3multiplier.click()
     time.sleep(2)
     driver.switch_to.alert.dismiss()
-    clickCookieFor(100)
+    clickCookieFor(bigCookie, 100)
     x5multiplier.click()
     time.sleep(2)
     driver.switch_to.alert.dismiss()
-    clickCookieFor(100)
+    clickCookieFor(bigCookie, 100)
     x2multiplier.click()
-    clickCookieFor(300)
+    clickCookieFor(bigCookie, 100)
 
     #check if final value matches and close test
     cookieJar = driver.execute_script("return getCookieJar();")
     if cookieJar == finalExpectedCookieJar:
-        print("------------- Text complete -------------")
+        print("------------- Test complete -------------")
         time.sleep(2)
         driver.quit()
 
     else:
         print("------------- The test failed -----------")
+        time.sleep(2)
         driver.quit()
         
-bigCookie = None
-def clickCookieFor(clicks):
+def clickCookieFor(bigCookie, clicks):
     for i in range(0, clicks):
         bigCookie.click()
 
+#debug code
 def run1():
     print("counter checker reporting")
 
-
+#run([300,600,300,0], 210)
